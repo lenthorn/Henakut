58f9814452b780b110dab37c26a94955eb5d233e
lenthorn <lenthorn592@gmail.com>
2013-02-16 15:35:31 +0000

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Henakut::Application.config.secret_token = '72620214786964a33fc02c8166fcfc7fd95cd57d4c30a47afed8e204cac817b9a54808c0019aeb14ea4d17e2f3b0eac405f03229d5e4c9ca3b3dee635ba6d8f8'
